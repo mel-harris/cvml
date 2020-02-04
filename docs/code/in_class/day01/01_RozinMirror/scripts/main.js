7794af93832feb594d83ae18d6fd6f0325f19101
mel-harris <60403005+mel-harris@users.noreply.github.com>
2020-02-04 03:36:39 -0600

    Update main.js

--- docs/code/in_class/day01/01_RozinMirror/scripts/main.js
+++ docs/code/in_class/day01/01_RozinMirror/scripts/main.js
@@ -4,11 +4,11 @@ let myCapture;
 
 function setup() {
   let canvas = createCanvas(640, 480);
-  
-  canvas.parent("sketch");
+
+
   myCapture = createCapture(VIDEO);
   myCapture.hide();
-  fill(255, 0, 0);
+  fill(255, 0, 255);
   noStroke();
 }
 
@@ -24,12 +24,16 @@ function draw() {
     // this says "don't do every single y pixel, only every x amount"
     for (let x = 0; x < width; x += stepSize) {
       const i = y * width + x;
-      // this turns these two dimensions into one
-      const darkness = (255 - myCapture.pixels[i*4]) / 255
+      // this turns these two dimensions into one 
+      const darkness = (255 - myCapture.pixels[i * 4]) / 255
       // the [i*4] reduces the 4 value information in RGBA to just one. this reduces the intake of information.
       const radius = stepSize * darkness;
-      // fill(250)
+   
       ellipse(x, y, radius, radius);
+        stroke(245, 0, 0);
+        strokeWeight(map(radius, 0, stepSize, 0, 3));
+        fill(map(radius, 0, stepSize, 0, 255), 0, 240, 240);
+      
     }
   }
 }
